989bb1f6fc056231983144bdfd65114d89d7de5b
ehatfie <ehhatfie@uark.edu>
2018-03-12 18:18:36 -0500

    updated object variable type

--- app/models/types/object.ts
+++ app/models/types/object.ts
@@ -4,7 +4,7 @@ import {MappedEntityBasicType} from './mappedEntityBasicType';
 
 export class Data extends MappedEntityBasicType{
     constructor(
-        public id: string = '',
+        public id: string = 'old',
         public amount: number = 0,
         public createdOn: moment.Moment = moment()) {
             super(id, createdOn);
